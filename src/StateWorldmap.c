e30d70d3275fd030b3a75287b94360c46c3b17bd
StudioLoading <38441612+StudioLoading@users.noreply.github.com>
2025-02-18 17:45:05 +0100

    worldmapping

---
+++ src/StateWorldmap.c
@@ -0,0 +1,12 @@
+#include "Banks/SetAutoBank.h"
+
+devo completare la worldmap.gbm modificando i tiles.
+poi fare le versioni con le macrozone accese
+poi questo State servirà a farle accendere
+
+void START() {
+}
+
+void UPDATE() {
+}
+
