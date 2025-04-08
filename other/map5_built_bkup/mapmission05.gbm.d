90f14f2920208a66660d977d8027b7eaa0e8ab1f
StudioLoading <38441612+StudioLoading@users.noreply.github.com>
2025-04-08 14:04:45 +0200

    adding stuff from /other folder

---
+++ other/map5_built_bkup/mapmission05.gbm.d
@@ -0,0 +1,4 @@
+../DbgClr/res/mapmission05.gbm.o: ../DbgClr/res/mapmission05.gbm.c \
+ C:/ZGB-2023.0/common/include/TilesInfo.h \
+ C:/ZGB-2023.0/common/include/MapInfo.h \
+ C:/ZGB-2023.0/common/include/TilesInfo.h
---
+++ other/map5_built_bkup/mapmission05.h
@@ -0,0 +1,8 @@
+#ifndef MAP_mapmission05_H
+#define MAP_mapmission05_H
+#define mapmission05Width 160
+#define mapmission05Height 80
+#include "MapInfo.h"
+extern unsigned char bank_mapmission05;
+extern struct MapInfo mapmission05;
+#endif
